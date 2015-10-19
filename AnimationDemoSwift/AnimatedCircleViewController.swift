4e9387931f53107fed33035a29ff09f3618a89b8
Coco <pszertlek@CocodeMacBook-Pro.local>
2015-10-19 09:13:30 +0800

    circle animation

--- AnimationDemoSwift/AnimatedCircleViewController.swift
+++ AnimationDemoSwift/AnimatedCircleViewController.swift
@@ -10,12 +10,31 @@ import UIKit
 
 class AnimatedCircleViewController: UIViewController {
 
+    @IBOutlet weak var mySlider: UISlider!
+    var cv:CircleView?
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        self.cv = nil
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        self.cv = nil
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        cv = CircleView(frame: CGRectMake(self.view.frame.size.width - CGFloat(160),self.view.frame.size.height / 2 - CGFloat(160),320,320))
+        self.view .addSubview(cv!)
+        cv!.circleLayer.progress = mySlider.value
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func sliderChange(sender: UISlider) {
+        cv!.circleLayer.progress = mySlider.value
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- AnimationDemoSwift/CircleLayer.swift
+++ AnimationDemoSwift/CircleLayer.swift
@@ -7,29 +7,37 @@
 //
 
 import UIKit
-
 class CircleLayer: CALayer {
 
     let outsideRectSize :Int32 = 90
-    var progress:Float{
-        set{
-            progress = newValue
-            if progress <= 0.5{
-                movePoint = 0
-                }
-            else{
-                movePoint = 1
-            }
-            lastProgress = progress
-            
-        }
-        get{
-            return progress
-        }
-    }
     var outsideRect:CGRect
     var lastProgress:Float
     var movePoint:Int32
+    var _progress:Float = 0.5
+    var progress:Float{
+        set(newValue){
+            _progress = newValue
+                if progress <= 0.5{
+                    movePoint = 0
+                }
+                else{
+                    movePoint = 1
+                }
+                lastProgress = progress
+            lastProgress = progress
+            let x = self.position.x - CGFloat(outsideRectSize)/2 + CGFloat(_progress - 0.5) * (self.frame.size.width - CGFloat(outsideRectSize))
+            let origin_y = self.position.y - CGFloat(outsideRectSize/2);
+            
+            self.outsideRect = CGRectMake(x, origin_y, CGFloat(outsideRectSize), CGFloat(outsideRectSize));
+            
+        self.setNeedsDisplay()
+
+        }
+        get{
+           return _progress
+        }
+    }
+
     
     
     override init(){
@@ -37,18 +45,75 @@ class CircleLayer: CALayer {
         lastProgress = 0
         movePoint = 0
         super.init()
-        progress = 0.5;
-
+        progress = 0.5
     }
     
     override func drawInContext(ctx: CGContext) {
         let offset = self.outsideRect.size.width / 3.6
-        let moveDistance = Float(outsideRect.size.width) * 1.0/6.0 * fabs(self.progress - 0.5)*2
+        let moveDistance = CGFloat(outsideRect.size.width) * 1.0/6.0 * CGFloat(fabs(self.progress - 0.5)*2)
         let rectCenter = CGPoint(x: self.outsideRect.origin.x + self.outsideRect.size.width / 2, y: self.outsideRect.origin.y + self.outsideRect.size.height / 2)
         let pointA = CGPoint(x: rectCenter.x, y: self.outsideRect.origin.y + CGFloat(moveDistance))
         let pointB = CGPoint(x: movePoint == 0 ? rectCenter.x + self.outsideRect.size.width / 2:rectCenter.x+self.outsideRect.size.height/2 - CGFloat(moveDistance), y: rectCenter.y)
         let pointC = CGPoint(x: rectCenter.x, y:rectCenter.y + self.outsideRect.size.height/2 - CGFloat(moveDistance))
-        let pointD = CGPointMake(self.movePoint == POINT_D ? self.outsideRect.origin.x - movedDistance*2 : self.outsideRect.origin.x, rectCenter.y);
+        let pointD = CGPointMake(self.movePoint == 0 ? self.outsideRect.origin.x - CGFloat(moveDistance)*2 : self.outsideRect.origin.x, rectCenter.y);
+        
+        let c1 = CGPointMake(pointA.x + offset, pointA.y);
+        let c2 = CGPointMake(pointB.x, self.movePoint == 0 ? pointB.y - offset : pointB.y - offset + moveDistance);
+        
+        let c3 = CGPointMake(pointB.x, self.movePoint == 0 ? pointB.y + offset : pointB.y + offset - moveDistance);
+        let c4 = CGPointMake(pointC.x + offset, pointC.y);
+        
+        let c5 = CGPointMake(pointC.x - offset, pointC.y);
+        let c6 = CGPointMake(pointD.x, self.movePoint == 0 ? pointD.y + offset - moveDistance : pointD.y + offset);
+        
+        let c7 = CGPointMake(pointD.x, self.movePoint == 0 ? pointD.y - offset + moveDistance : pointD.y - offset);
+        let c8 = CGPointMake(pointA.x - offset, pointA.y);
+        let rectPath = UIBezierPath(rect: outsideRect)
+        
+        CGContextAddPath(ctx, rectPath.CGPath)
+        CGContextSetStrokeColorWithColor(ctx, UIColor.blackColor().CGColor)
+        CGContextSetLineWidth(ctx, 1.0)
+//        let dash = [5.0,5.0]
+//        CGContextSetLineDash(ctx, 0.0, dash, 2)
+        CGContextStrokePath(ctx)
+        
+        let ovalPath = UIBezierPath()
+        ovalPath .moveToPoint(pointA)
+        ovalPath.addCurveToPoint(pointB, controlPoint1: c1, controlPoint2: c2)
+        ovalPath.addCurveToPoint(pointC, controlPoint1: c3, controlPoint2: c4)
+        ovalPath.addCurveToPoint(pointD, controlPoint1: c5, controlPoint2: c6)
+        ovalPath.addCurveToPoint(pointA, controlPoint1: c7, controlPoint2: c8)
+        
+        ovalPath .closePath()
+        
+        CGContextAddPath(ctx, ovalPath.CGPath)
+        CGContextSetStrokeColorWithColor(ctx, UIColor.blackColor().CGColor)
+        CGContextSetFillColorWithColor(ctx, UIColor.redColor().CGColor)
+        CGContextSetLineDash(ctx, 0, nil, 0)
+        CGContextDrawPath(ctx,.FillStroke)
+        
+        let helperPath = UIBezierPath()
+        helperPath.moveToPoint(pointA)
+        helperPath.addLineToPoint(c1)
+        helperPath.addLineToPoint(c2)
+        helperPath.addLineToPoint(pointB)
+        helperPath.addLineToPoint(c3)
+        helperPath.addLineToPoint(c4)
+        helperPath.addLineToPoint(pointC)
+        helperPath.addLineToPoint(c5)
+        helperPath.addLineToPoint(c6)
+        helperPath.addLineToPoint(pointD)
+        helperPath.addLineToPoint(c7)
+        helperPath.addLineToPoint(c8)
+        helperPath .closePath()
+        
+        
+        CGContextAddPath(ctx, helperPath.CGPath);
+        
+//        CGFloat dash2[] = {2.0, 2.0};
+//        CGContextSetLineDash(ctx, 0.0, dash2, 2);
+        CGContextStrokePath(ctx); //给辅助线条填充颜色
+
     }
     
 //    init(layer:CircleLayer){
